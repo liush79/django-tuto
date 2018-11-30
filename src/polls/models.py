cf38f187dc66078fafbac94ab077469a2d7e9988
seungho.ryu <98koom@hanmail.net>
2018-11-30 17:24:05 +0900

    Completed part 2.

--- src/polls/models.py
+++ src/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+# django admin user: admin/qwer1234
+class Question(models.Model):
+    qustion_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
